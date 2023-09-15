46d7299e63c7ee9011f27a2ebfeab646a430ec2f
Antoine Buquet <antoine.buquet49@gmail.com>
2023-09-15 14:28:49 +0200

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { IdleComponent } from './components/idle/idle.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'idle', component: IdleComponent },
+  { path: '', redirectTo: '/idle', pathMatch: 'full' }, // Page par défaut
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IdleComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/idle/idle.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-idle',
+  templateUrl: './idle.component.html',
+  styleUrls: ['./idle.component.css']
+})
+export class IdleComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  redirectToHome() {
+    this.router.navigate(['/home']);
+  }
+}
