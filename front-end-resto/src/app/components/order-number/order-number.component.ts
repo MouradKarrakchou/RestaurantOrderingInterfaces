b7c07807936870ba4c194ffd0beab93902086f08
Ayoub <youbima17@gmail.com>
2023-09-29 14:07:10 +0200

    time added #12

--- front-end-resto/src/app/components/order-number/order-number.component.ts
+++ front-end-resto/src/app/components/order-number/order-number.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-order-number',
   templateUrl: './order-number.component.html',
-  styleUrls: ['./order-number.component.css']
+  styleUrls: ['./order-number.component.css'],
 })
 export class OrderNumberComponent implements OnInit {
+
+  time: Date | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.time = new Date();
+    }, 1000);
   }
 
 }
