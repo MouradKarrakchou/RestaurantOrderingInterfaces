7255fa01aba28b90de9a28fa3f588598a665f006
Ayoub <youbima17@gmail.com>
2023-10-08 10:16:01 +0200

    quit button designed #28

--- front-end-resto/src/app/components/order-number/order-number.component.ts
+++ front-end-resto/src/app/components/order-number/order-number.component.ts
@@ -59,7 +59,7 @@ export class OrderNumberComponent implements OnInit {
 
     setTimeout(() => {
       this.router.navigate(['/idle'])
-    }, 20000)
+    }, 20000);
   }
 
   sendOrderToBFF(): Observable<any> {
@@ -251,6 +251,7 @@ export class OrderNumberComponent implements OnInit {
   }
 
   quit(): void {
+    clearTimeout();
     this.router.navigate(['/idle'])
   }
 
