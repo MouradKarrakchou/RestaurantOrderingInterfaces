e9dc447c1df41c2a54eb44f4bca90d4a50d3066b
MouradKarrakchou <91179433+MouradKarrakchou@users.noreply.github.com>
2023-11-10 19:31:31 +0100

    global paiment page and refractoring #35

--- front-end-resto-table/src/app/services/payment.service.ts
+++ front-end-resto-table/src/app/services/payment.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import BasketItem from "../models/BasketItem";
+import {BasketService} from "./basket.service";
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +9,14 @@ export class PaymentService {
 
   groupPayment: boolean = false;
 
-  constructor() { }
+  alreadyPaid: { [key: string]: boolean } = {
+    '1': false,
+    '2': false,
+    '3': false,
+    '4': false,
+  };
+
+  constructor(private basketService: BasketService) { }
 
   setGroupPayment(groupPayment: boolean) {
     this.groupPayment = groupPayment;
@@ -15,5 +24,18 @@ export class PaymentService {
 
   getGroupPayment() {
     return this.groupPayment;
+  }
+
+  setPaid(tabletId: string) {
+    this.alreadyPaid[tabletId] = true;
+    //check if evryone has paid
+    let everyonePaid = true;
+    let listOfCustomer= this.basketService.getAllTabletteActivated();
+    for (const key of listOfCustomer) {
+      if (!this.alreadyPaid[key]) {
+        everyonePaid = false;
+      }
+    }
+    //TODO : if everyone has paid then reset everything
   }
 }
