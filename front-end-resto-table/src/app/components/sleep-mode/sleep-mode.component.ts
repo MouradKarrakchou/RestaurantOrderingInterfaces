bae6e92b25545ba85fbdbb627d5506a4d30b04c6
Ayoub <youbima17@gmail.com>
2023-11-10 19:47:55 +0100

    handle action only if powered on #34

--- front-end-resto-table/src/app/components/sleep-mode/sleep-mode.component.ts
+++ front-end-resto-table/src/app/components/sleep-mode/sleep-mode.component.ts
@@ -36,74 +36,78 @@ export class SleepModeComponent implements OnInit {
 
   selectTab(tabNumber: string) {
     //TODO turn on/off the other tables
-    switch (tabNumber) {
-      case 'tab1':
-        this.tab1.nativeElement.style.background = this.tab1.nativeElement.style.background == 'rgb(112, 147, 112)' ? 'rgb(169, 169, 169)' : 'rgb(112, 147, 112)';
-        this.tab1Selected = !this.tab1Selected;
-        break;
-      case 'tab2':
-        this.tab2.nativeElement.style.background = this.tab2.nativeElement.style.background == 'rgb(112, 147, 112)' ? 'rgb(169, 169, 169)' : 'rgb(112, 147, 112)';
-        this.tab2Selected = !this.tab2Selected;
-        break;
-      case 'tab3':
-        this.tab3.nativeElement.style.background = this.tab3.nativeElement.style.background == 'rgb(112, 147, 112)' ? 'rgb(169, 169, 169)' : 'rgb(112, 147, 112)';
-        this.tab3Selected = !this.tab3Selected;
-        break;
-      case 'tab4':
-        this.tab4.nativeElement.style.background = this.tab4.nativeElement.style.background == 'rgb(112, 147, 112)' ? 'rgb(169, 169, 169)' : 'rgb(112, 147, 112)';
-        this.tab4Selected = !this.tab4Selected;
-        break;
+    if (this.powerUp) {
+      switch (tabNumber) {
+        case 'tab1':
+          this.tab1.nativeElement.style.background = this.tab1.nativeElement.style.background == 'rgb(112, 147, 112)' ? 'rgb(169, 169, 169)' : 'rgb(112, 147, 112)';
+          this.tab1Selected = !this.tab1Selected;
+          break;
+        case 'tab2':
+          this.tab2.nativeElement.style.background = this.tab2.nativeElement.style.background == 'rgb(112, 147, 112)' ? 'rgb(169, 169, 169)' : 'rgb(112, 147, 112)';
+          this.tab2Selected = !this.tab2Selected;
+          break;
+        case 'tab3':
+          this.tab3.nativeElement.style.background = this.tab3.nativeElement.style.background == 'rgb(112, 147, 112)' ? 'rgb(169, 169, 169)' : 'rgb(112, 147, 112)';
+          this.tab3Selected = !this.tab3Selected;
+          break;
+        case 'tab4':
+          this.tab4.nativeElement.style.background = this.tab4.nativeElement.style.background == 'rgb(112, 147, 112)' ? 'rgb(169, 169, 169)' : 'rgb(112, 147, 112)';
+          this.tab4Selected = !this.tab4Selected;
+          break;
+      }
     }
   }
 
   selectPaymentMethod(paymentMethod: string) {
-    if ((this.together.nativeElement.style.background == ''
-      && this.separately.nativeElement.style.background == '')
-      || (this.together.nativeElement.style.background == 'rgb(169, 169, 169)'
-        && this.separately.nativeElement.style.background == 'rgb(169, 169, 169)')
-      || (this.together.nativeElement.style.background == ''
-        && this.separately.nativeElement.style.background == 'rgb(169, 169, 169)')
-      || (this.together.nativeElement.style.background == 'rgb(169, 169, 169)'
-        && this.separately.nativeElement.style.background == '')) {
-      console.log("if")
-      switch (paymentMethod) {
-        case 'together': this.together.nativeElement.style.background = 'rgb(112, 147, 112)'; break;
-        case 'separately': this.separately.nativeElement.style.background = 'rgb(112, 147, 112)'; break;
+    if (this.powerUp) {
+      if ((this.together.nativeElement.style.background == ''
+              && this.separately.nativeElement.style.background == '')
+          || (this.together.nativeElement.style.background == 'rgb(169, 169, 169)'
+              && this.separately.nativeElement.style.background == 'rgb(169, 169, 169)')
+          || (this.together.nativeElement.style.background == ''
+              && this.separately.nativeElement.style.background == 'rgb(169, 169, 169)')
+          || (this.together.nativeElement.style.background == 'rgb(169, 169, 169)'
+              && this.separately.nativeElement.style.background == '')) {
+        console.log("if")
+        switch (paymentMethod) {
+          case 'together':
+            this.together.nativeElement.style.background = 'rgb(112, 147, 112)';
+            break;
+          case 'separately':
+            this.separately.nativeElement.style.background = 'rgb(112, 147, 112)';
+            break;
+        }
+        this.paymentMethodSelected = paymentMethod;
+      } else if (this.together.nativeElement.style.background == 'rgb(112, 147, 112)'
+          && paymentMethod == this.paymentMethodSelected) {
+        this.together.nativeElement.style.background = 'rgb(169, 169, 169)';
+        this.paymentMethodSelected = '';
+      } else if (this.separately.nativeElement.style.background == 'rgb(112, 147, 112)'
+          && paymentMethod == this.paymentMethodSelected) {
+        this.separately.nativeElement.style.background = 'rgb(169, 169, 169)';
+        this.paymentMethodSelected = '';
+      } else if ((this.together.nativeElement.style.background == 'rgb(112, 147, 112)'
+              || this.together.nativeElement.style.background == '')
+          && (this.separately.nativeElement.style.background == 'rgb(169, 169, 169)'
+              || this.separately.nativeElement.style.background == '')
+          && paymentMethod != this.paymentMethodSelected) {
+        console.log("else if")
+        this.together.nativeElement.style.background = 'rgb(169, 169, 169)';
+        this.separately.nativeElement.style.background = 'rgb(112, 147, 112)';
+        this.paymentMethodSelected = paymentMethod;
+      } else if ((this.together.nativeElement.style.background == 'rgb(169, 169, 169)'
+              || this.together.nativeElement.style.background == '')
+          && (this.separately.nativeElement.style.background == 'rgb(112, 147, 112)'
+              || this.separately.nativeElement.style.background == '')
+          && paymentMethod != this.paymentMethodSelected) {
+        console.log("else if 2")
+        this.together.nativeElement.style.background = 'rgb(112, 147, 112)';
+        this.separately.nativeElement.style.background = 'rgb(169, 169, 169)';
+        this.paymentMethodSelected = paymentMethod;
       }
-      this.paymentMethodSelected = paymentMethod;
-    }
-    else if (this.together.nativeElement.style.background == 'rgb(112, 147, 112)'
-      && paymentMethod == this.paymentMethodSelected) {
-      this.together.nativeElement.style.background = 'rgb(169, 169, 169)';
-      this.paymentMethodSelected = '';
-    }
-    else if (this.separately.nativeElement.style.background == 'rgb(112, 147, 112)'
-      && paymentMethod == this.paymentMethodSelected) {
-      this.separately.nativeElement.style.background = 'rgb(169, 169, 169)';
-      this.paymentMethodSelected = '';
-    }
-    else if ((this.together.nativeElement.style.background == 'rgb(112, 147, 112)'
-        || this.together.nativeElement.style.background == '')
-      && (this.separately.nativeElement.style.background == 'rgb(169, 169, 169)'
-        || this.separately.nativeElement.style.background == '')
-      && paymentMethod != this.paymentMethodSelected) {
-      console.log("else if")
-      this.together.nativeElement.style.background = 'rgb(169, 169, 169)';
-      this.separately.nativeElement.style.background = 'rgb(112, 147, 112)';
-      this.paymentMethodSelected = paymentMethod;
-    }
-    else if ((this.together.nativeElement.style.background == 'rgb(169, 169, 169)'
-        || this.together.nativeElement.style.background == '')
-      && (this.separately.nativeElement.style.background == 'rgb(112, 147, 112)'
-        || this.separately.nativeElement.style.background == '')
-      && paymentMethod != this.paymentMethodSelected) {
-      console.log("else if 2")
-      this.together.nativeElement.style.background = 'rgb(112, 147, 112)';
-      this.separately.nativeElement.style.background = 'rgb(169, 169, 169)';
-      this.paymentMethodSelected = paymentMethod;
-    }
 
-    this.checkValidation();
+      this.checkValidation();
+    }
   }
 
   power() {
@@ -117,14 +121,18 @@ export class SleepModeComponent implements OnInit {
   }
 
   validatePayment() {
-    //TODO redirection ?? Bill ??
+    if (this.powerUp && this.checkValidation()) {
+      //TODO redirection ?? Bill ??
+    }
   }
 
   checkValidation() {
     if (this.paymentMethodSelected == 'together' || this.paymentMethodSelected == 'separately') {
       this.validate.nativeElement.style.background = 'rgb(112, 147, 112)';
+      return true;
     } else {
       this.validate.nativeElement.style.background = 'rgb(169, 169, 169)';
+      return false;
     }
   }
 }
