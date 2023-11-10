3bd2ebebdfec9d8b70c0fb584227d7a896f73adb
Benoit Gaudet <bgaudet38@gmail.com>
2023-11-10 17:32:17 +0100

    Fix merge #37

--- front-end-resto-table/src/app/app.module.ts
+++ front-end-resto-table/src/app/app.module.ts
@@ -48,7 +48,7 @@ const routes: Routes = [
   { path: 'client-receipt/:id', component: ClientReceiptComponent },
   { path: 'end/:id', component: EndComponent },
   { path: 'status', component: PreparationStatusComponent},
-  { path: 'wait', component: WaitingScreenComponent},
+  { path: 'waiting-screen', component: WaitingScreenComponent},
   { path: 'summary/:id', component: GlobalBasketSummaryComponent },
   { path: '**', redirectTo: '/table-viewer' } // GARDER TOUT EN BAS
 ];
@@ -75,6 +75,8 @@ const routes: Routes = [
     ClientReceiptComponent,
     MiddleTableComponent,
     EndComponent,
+    WaitingScreenComponent,
+    GameLeaderboardComponent,
   ],
     imports: [
         BrowserModule,
